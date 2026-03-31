3c0055dab0d14e71d1bf73680a5f26ec03695d9d
Zyara-1ot <sabitaapal1976@gmail.com>
2026-03-31 15:27:45 +0530

    --

---
+++ src/central_pool.cpp
@@ -0,0 +1,19 @@
+#include "central_pool.h"
+#include <iostream>
+#include <cstddef>
+#include <mutex>
+
+central_pool::central_pool(){
+
+};
+
+void* central_pool::allocate(int class_idx){
+    std::lock_guard<std::mutex> lock(locks[class_idx]);  // lock th mtx for tis size class
+    void* ptr = lists[class_idx].pop(); // then pop
+    return ptr; // and rturn ptr
+};
+
+void central_pool::free(int class_idx, void* ptr){
+    std::lock_guard<std::mutex> lock(locks[class_idx]); // lock ,, pus free block back,,auto unlock 
+    lists[class_idx].push(ptr);
+};
