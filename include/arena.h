6bab388d007b044225edc0d01f2f86eba1e44fab
Zyara-1ot <sabitaapal1976@gmail.com>
2026-03-31 15:29:16 +0530

    --

--- include/arena.h
+++ include/arena.h
@@ -1,22 +1,22 @@
 #ifndef ARENA_H
 #define ARENA_H
 
-#include <cstdint>
 #include <cstddef>
-#include <atomic>
 
-class arena{
-private :
-  char* start;
-  size_t total_size;
-  std::atomic<size_t>offset;
-
+class arena {
+private:
+    char* start; // start of 1gb block
+    size_t offset; //bump ptr to current position 
+    size_t total_size; // total 1gb always
 public:
-  arena();
-  void* allocate(size_t size);
-  ~arena();
-  
+    arena();
+    __attribute__((always_inline)) inline void* allocate(size_t size) { //--no func call overhead at all,,code gets pasted directly in the call site ,,
+        size_t aligned = ((size + 63) / 64) * 64; // roundoff to 64 byte ,,, 
+        void* ptr = start + offset; // aligned with cache line
+        offset += aligned; //e evry allocation starts at cache line boundary ,, 
+        return ptr;
+    }
+    ~arena();
 };
 
 #endif
-
